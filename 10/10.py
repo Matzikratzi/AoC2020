f73efe8c86591d775da903e17730b6f907cbe97b
Mats Jargenstedt <mats.jargenstedt@saabgroup.com>
2020-12-10 13:33:32 +0100

    Solution for day 10, second problem

--- 10/10.py
+++ 10/10.py
@@ -1,14 +1,30 @@
 #!/usr/bin/python3
 
-with open('input-sorted') as f:
+with open('input') as f:
 #with open('small-input') as f:
-    data = [line.rstrip() for line in f]
+    data = [int(line.rstrip()) for line in f]
+data.sort()
+data.append(data[-1] + 3)
 
-numDiff1 = 1
-numDiff3 = 1
-i = 1
+diffBins = [0, 0, 0, 0, 0, 0]
+diffData = []
+numDiff1 = 0
+numDiff3 = 0
+i = 0
+prev = 0
+lastDiff = 0
+sameDiff = 0
 while i < len(data):
-    diff = int(data[i]) - int(data[i-1])
+    current = data[i]
+    diff = current - prev
+    if diff == lastDiff:
+        sameDiff += 1
+    else:
+        if lastDiff == 1:
+            diffBins[sameDiff] += 1
+        lastDiff = diff
+        sameDiff = 1
+    diffData.append(diff)
     if diff == 1:
         numDiff1 += 1
     elif diff == 3:
@@ -16,14 +32,16 @@ while i < len(data):
     else:
         print(diff)
     i += 1
-print(numDiff1)
-print(numDiff3)
+    prev = current
+
 print('First Answer:',numDiff3 * numDiff1)
+# This is calculated by hand 
+pathsInGroup = [0, 1, 2, 4, 7]
 
-##################################################
-# Second solution
-
-with open('input-sorted') as f:
-#with open('small-input') as f:
-    data = [line.rstrip() for line in f]
-
+numOfValidPaths = 1
+i = 0
+while i < len(pathsInGroup):
+    numOfValidPaths *= pathsInGroup[i]**diffBins[i]
+    i += 1
+    
+print('Second Answer:', numOfValidPaths)
