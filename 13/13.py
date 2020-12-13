9ef82936ff348796d7f075c674ca48d3be1b2d51
Mats Jargenstedt <mats@jargenstedt.se>
2020-12-13 09:46:13 +0100

    13: part 1

---
+++ 13/13.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+import math
+
+with open('input') as f:
+#with open('input-small') as f:
+    data = [line.rstrip() for line in f]
+
+earliest = int(data[0])
+IDs = data[1].split(',')
+
+workingIDs = [int(x) for x in IDs if x != 'x']
+
+print(workingIDs)
+
+print(earliest)
+
+
+winnerDeparture = 100000000000000000000000000000
+winnerID        = -1
+
+print(max(workingIDs))
+for id in workingIDs:
+    earlierDepart = math.floor(earliest/id) * id
+    earliestDepart = earlierDepart + id
+    print(id, earlierDepart, earliestDepart)
+    if earliestDepart < winnerDeparture:
+        winnerDeparture = earliestDepart
+        winnerID = id
+
+print(winnerID, winnerDeparture, winnerID*(winnerDeparture-earliest))
