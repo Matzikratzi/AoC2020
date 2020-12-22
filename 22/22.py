df23813cb4099536fe59aef1aef754656a8831a8
Mats Jargenstedt <mats.jargenstedt@saabgroup.com>
2020-12-22 23:06:21 +0100

    Wip

--- 22/22.py
+++ 22/22.py
@@ -4,8 +4,8 @@ import re
 # Function that reads the input file and initiates the cards for the
 # players
 def Dealer():
-    #with open('input') as f:
-    with open('input-small') as f:
+    with open('input') as f:
+    # with open('input-smallest') as f:
         data = [line.rstrip() for line in f]
 
     hP1 = []
@@ -65,46 +65,75 @@ def Part1():
 
     print('Part 1 Winning Score:', points)
 
-Part1()
+# Part1()
 game = 1
+
+def GameIsPlayed(d1, d2, previousDecks):
+    # print(playedGames)
+    for game in previousDecks:
+        oldD1 = game[0]
+        oldD2 = game[1]
+        if d1 == oldD1 and d2 == oldD2:
+            return True, previousDecks
+
+    previousDecks.append([d1.copy(), d2.copy()])
+    return False, previousDecks
+
 # Function that decides the winnig card each part 1 round
 def RecCombat(d1, d2, gameNum):
     global game
+    previousDecks = []
     runda = 1
     runBefore = False
     while True:
-        print('-- Round', runda, '(Game', gameNum,') --')
-        print('Player 1\'s deck:', d1)
-        print('Player 2\'s deck:', d2)
-        if runBefore:
-            return 1
+        if d1 == []:
+            # print('The winner of game', gameNum, 'is player', 2)
+            return 2, d2
+        elif d2 == []:
+            # print('The winner of game', gameNum, 'is player', 1)
+            return 1, d1
         else:
-            if d1 == []:
-                return 2, d2
-            elif d2 == []:
-                return 1, d1
+            # print('-- Round', runda, '(Game', gameNum,') --')
+            # print('Player 1\'s deck:', d1)
+            # print('Player 2\'s deck:', d2)
+            again, previousDecks = GameIsPlayed(d1, d2, previousDecks)
+            if again:
+                print('Same game again!')
+                print('The winner of game', gameNum, 'is player', 1)
+                print('deck 1:', d1)
+                points = 0
+                i = 0
+                iMax = len(d1)
+                while i < iMax:
+                    points += d1[i]*(iMax-i)
+                    i += 1
+                print('Part 2 Winning Score:', points)
+
+                return 1, d1, True
             else:
                 c1 = d1.pop(0)
                 c2 = d2.pop(0)
-                print('Player 1 plays:', c1)
-                print('Player 2 plays:', c2)
+                # print('Player 1 plays:', c1)
+                # print('Player 2 plays:', c2)
 
                 if c1 <= len(d1) and c2 <= len(d2):
                     # recursive!
-                    print('Playing a sub-game to determine the winner.')
+                    # print('Playing a sub-game to determine the winner.')
                     game += 1
                     winner = RecCombat(d1[:c1], d2[:c2], game-1)
+                    # print('\n...anyway, back to game', gameNum)
                 else:
                     if c1 > c2:
                         winner = 1
                     else:
                         winner = 2
                         
-                if winner == 1:
-                    print('Player 1 wins round', runda,' of game,', gameNum)
-                    d1.extend([c1, c2])
-                else:
-                    d2.extend([c2, c1])
+        if winner == 1:
+            # print('Player 1 wins round', runda,' of game,', gameNum)
+            d1.extend([c1, c2])
+        else:
+            # print('Player 2 wins round', runda,' of game,', gameNum)
+            d2.extend([c2, c1])
         runda += 1
 
 def Part2():
@@ -122,6 +151,7 @@ def Part2():
         points += deck[i]*(iMax-i)
         i += 1
 
-    print('Part 1 Winning Score:', points)
-
+    print('Part 2 Winning Score:', points)
+    print('Winner:', winner)
+    print('Deck:', deck)
 Part2()
